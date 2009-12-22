3bb3ea67dba8a36cbc76f470fb0e4c61259c058c
jbapple <jbapple@40504ecf-d755-0410-b977-ff30fbdddbd2>
2009-12-22 03:21:52 +0000

    parameterize proof over definition of iterate

--- Data/Induct.v
+++ Data/Induct.v
@@ -11,6 +11,8 @@ Require Import Coq.Program.Equality.
 Section type.
 
 Variable A:Set.
+Variable (Aeq : relation A).
+Variable (Aeq_equiv : Equivalence Aeq).
 
 CoInductive Braun : Set :=
 | Conb : A -> Braun -> Braun -> Braun.
@@ -23,7 +25,7 @@ It is often impossible to prove inductive equality for coinductive objects like 
 
 CoInductive coeq : Braun -> Braun -> Prop :=
 | co : forall x y od od' ev ev',
-        (x = y) -> coeq od od' -> coeq ev ev'
+        (Aeq x y) -> coeq od od' -> coeq ev ev'
         -> coeq (Conb x od ev) (Conb y od' ev').
 
 (*
@@ -32,21 +34,130 @@ Here begins the definition of the algorithm.
 
 *)
 
+(*
+Definition map := {F : A -> A | forall x y, Aeq x y -> Aeq (F x) (F y)}.
+Program Definition ap (F:map) (x:A) : A :=
+  F x.
+Print ap.
+*)
+
+Definition fmorph f := forall x y, Aeq x y -> Aeq (f x) (f y).
+
+Hint Unfold fmorph.
+
 CoFixpoint fmap F (x:Braun) : Braun :=
   match x with
     | Conb h od ev => Conb (F h) (fmap F od) (fmap F ev) 
   end.
+
+
+Definition frob (x : Braun) : Braun :=
+match x with
+| Conb h od ev => Conb h od ev
+end.
+
+Lemma frobeq : forall (x:Braun), x = frob x.
+Proof.
+  destruct x. simpl. reflexivity.
+Defined.
+
+Definition aext f g :=
+  forall x y,
+    Aeq x y ->
+    Aeq (f x) (g y).
+
+Lemma fmapMorph :
+  forall f g x y,
+    aext f g->
+    coeq x y ->
+    coeq (fmap f x) (fmap g y).
+Proof.
+  cofix.
+  destruct x; destruct y;
+    simpl;
+      unfold aext; intros.
+  rewrite (frobeq (fmap f _)).
+  rewrite (frobeq (fmap g _)). 
+  simpl.
+  inversion H0; subst.
+  constructor.
+  apply H; apply H5.
+  apply fmapMorph. exact H. exact H8.
+  apply fmapMorph. exact H. exact H9.
+Qed.
+
 (*
   Conb 
   (match x with | Conb h _ _ => F h end)
   (match x with | Conb _ o _ => fmap F o end)
   (match x with | Conb _ _ e => fmap F e end).
 *)
-
+(*
 CoFixpoint oddFromEven F (x:A) (b:Braun) : Braun :=
   match b with
     | Conb h od ev => Conb x (oddFromEven F (F h) ev) (fmap F od)
   end.
+*)
+
+
+Definition odds (x:Braun) :=
+  match x with
+    | Conb _ od _ => od
+  end.
+
+Definition evens (x:Braun) :=
+  match x with
+    | Conb _ _ ev => ev
+  end.
+
+Definition bead (x:Braun) :=
+  match x with
+    | Conb h _ _ => h
+  end.
+
+Add Parametric Morphism : bead with
+  signature 
+  coeq ==> Aeq as beadMor.
+Proof.
+  intros.
+  destruct x; destruct y.
+  simpl.
+  inversion H. assumption.
+Qed.
+
+Add Parametric Morphism : odds with
+  signature 
+  coeq ==> coeq as oddsMor.
+Proof.
+  intros.
+  destruct x; destruct y.
+  simpl.
+  inversion H. assumption.
+Qed.
+
+Add Parametric Morphism : evens with
+  signature 
+  coeq ==> coeq as evensMor.
+Proof.
+  intros.
+  destruct x; destruct y.
+  simpl.
+  inversion H. assumption.
+Qed.
+
+Variable oddFromEven : (A->A) -> A -> Braun -> Braun.
+Variable oddFromUnfold :
+  forall F x b,
+    coeq (oddFromEven F x b) 
+    (Conb x (oddFromEven F (F (bead b)) (evens b)) (fmap F (odds b))).
+Variable oddFromMorph :
+  forall f g x y v w,
+    aext f g ->
+    Aeq x y ->
+    coeq v w ->
+    coeq (oddFromEven f x v) 
+         (oddFromEven g y w).
+
 (*
   Conb x 
   (match b with 
@@ -65,16 +176,44 @@ It is difficult to define 'od' in such a way that Coq can see that it is product
 Fact od : (A -> A) -> A -> Braun
 with ev : (A -> A) -> A -> Braun.
 *)
-Variable od : (A->A) -> A -> Braun.
+Variable od : (A -> A) -> A -> Braun.
+Variable ev : (A -> A) -> A -> Braun.
 
-Definition ev F (x:A) :=
-  fmap F (od F x).
-
-Variable odeq : forall F (x:A), 
+Variable odMorph :
+  forall f g x y,
+    aext f g ->
+    Aeq x y ->
+    coeq (od f x) (od g y).
+Variable evMorph :
+  forall f g x y,
+    aext f g ->
+    Aeq x y ->
+    coeq (ev f x) (ev g y).
+    
+Variable odUnfold : forall F (x:A), 
   coeq (od F x) (oddFromEven F (F x) (ev F x)).
+Variable evUnfold : forall F x,
+  coeq (ev F x) (fmap F (od F x)).
 
 Definition iterate F (x:A) : Braun :=
   Conb x (od F x) (ev F x).
+
+Add Parametric Morphism : iterate with
+  signature aext ==> Aeq ==> coeq as iterateMorph.
+Proof.
+  cofix.
+  unfold aext in *; 
+    unfold iterate in *; 
+      intros.
+  constructor.
+  assumption.
+  apply odMorph.
+  assumption.
+  assumption.
+  apply evMorph.
+  assumption.
+  assumption.
+Qed.
 
 (*
 
@@ -126,7 +265,7 @@ Fixpoint bat (x:Braun) (b:list bool) {struct b} : A :=
   end.
 
 Lemma batcoeq : forall x y,
-  (forall b, bat x b = bat y b) -> coeq x y.
+  (forall b, Aeq (bat x b) (bat y b)) -> coeq x y.
 Proof.
   cofix; intros.
   destruct x; destruct y; constructor.
@@ -136,7 +275,7 @@ Proof.
 Qed.
 
 Add Parametric Morphism : bat with
-  signature coeq ==> (@eq (list bool)) ==> (@eq A) as batmor.
+  signature coeq ==> (@eq (list bool)) ==> (Aeq) as batmor.
 Proof.
   intros ? ? ? b; 
     generalize dependent x;
@@ -145,14 +284,14 @@ Proof.
           destruct x; destruct y; 
             inversion xy; subst.
 
-  reflexivity.
+  simpl. assumption.
 
   destruct a; apply IHb; assumption.
 Qed.
 
-Check batmor_Morphism.
+Check batmor.
 
-Hint Rewrite odeq using batmor_Morphism : core.
+Hint Rewrite odUnfold using batmor_Morphism : core.
 
 (*
 
@@ -175,37 +314,20 @@ Fixpoint applyn (n:nat) (f:A->A) (x:A) : A :=
     | S m => f (applyn m f x)
   end.
 
+Add Parametric Morphism : applyn with
+  signature (@eq nat) ==> aext ==> aext as applynMorph.
+Proof.
+  unfold aext in *;
+    induction y;
+      intros.
+  simpl. assumption.
+  simpl.
+  apply H.
+  apply IHy.
+  assumption. assumption.
+Qed. 
+
 Hint Unfold applyn.
-
-Definition odds (x:Braun) :=
-  match x with
-    | Conb _ od _ => od
-  end.
-
-Definition evens (x:Braun) :=
-  match x with
-    | Conb _ _ ev => ev
-  end.
-
-Add Parametric Morphism : odds with
-  signature 
-  coeq ==> coeq as oddsMor.
-Proof.
-  intros.
-  destruct x; destruct y.
-  simpl.
-  inversion H. assumption.
-Qed.
-
-Add Parametric Morphism : evens with
-  signature 
-  coeq ==> coeq as evensMor.
-Proof.
-  intros.
-  destruct x; destruct y.
-  simpl.
-  inversion H. assumption.
-Qed.
 
 Definition bacc x y := ord x < ord y.
 
@@ -354,7 +476,10 @@ Proof.
     generalize dependent f;
       induction n; intros.
   auto.
-  simpl. rewrite IHn. apply applyAdd.
+  simpl.
+  rewrite <- applyAdd.
+  f_equal.
+  apply IHn.
 Qed.
 
 Hint Rewrite applyMul : arith.
@@ -486,18 +611,71 @@ oddFromEven f (f^(2^k-1) x) even = iterate f^(2^k) (f^(2^k-1) x)
 *)
 
 
+Lemma coeq_refl : forall x, coeq x x.
+Proof.
+  cofix.
+  destruct x. 
+  constructor.
+  setoid_reflexivity.
+  apply coeq_refl.
+  apply coeq_refl.
+Qed.
+
+Lemma coeq_symm : forall x y, coeq x y -> coeq y x.
+Proof.
+  cofix.
+  intros x y coeq_x_y.
+  destruct coeq_x_y.
+  constructor. 
+  setoid_symmetry. assumption.
+  apply coeq_symm. assumption.
+  apply coeq_symm. assumption.
+Qed.
+
+Lemma coeq_trans : forall x y z , coeq x y -> coeq y z -> coeq x z.
+Proof.
+  cofix.
+  intros x y z coeq_x_y coeq_y_z.
+  inversion coeq_x_y as [xh  yh xod  yod xev  yev].
+  inversion coeq_y_z as [yh' zh yod' zod yev' zev yzh yzod yzev yy].
+  subst.
+  inversion yy. subst. clear yy.
+  constructor.
+  setoid_transitivity yh; assumption.
+  apply coeq_trans with yod; assumption.
+  apply coeq_trans with yev; assumption.
+Qed.
+
+Add Parametric Relation : Braun coeq
+reflexivity proved by coeq_refl
+symmetry proved by coeq_symm
+transitivity proved by coeq_trans
+as coeq_equiv.
+(*
+Lemma eqco :
+  forall x,
+    coeq x x.
+Proof.
+  cofix.
+  intros; destruct x; constructor.
+  reflexivity.
+  apply eqco.
+  apply eqco.
+Qed.
+*)
 Lemma mainLemma :
   forall b e x f k,
+    aext f f ->
     (forall j, ord j < ord b ->
-      bat e j = 
-      applyn (ord j)
+      Aeq (bat e j)
+      (applyn (ord j)
       (applyn (pow 2 k) f) 
-      (applyn (sub (*(pow 2 (k+1)) 2*) (dec2pow k)) f x)) ->
-    bat (oddFromEven f 
-      (applyn (sub (*(pow 2 k) 1*) (dec1pow k)) f x) e) b =
+      (applyn (sub (*(pow 2 (k+1)) 2*) (dec2pow k)) f x))) ->
+    Aeq (bat (oddFromEven f 
+      (applyn (sub (*(pow 2 k) 1*) (dec1pow k)) f x) e) b) (
     applyn (ord b)
     (applyn (pow 2 k) f) 
-    (applyn (sub (*(pow 2 k) 1*) (dec1pow k)) f x).
+    (applyn (sub (*(pow 2 k) 1*) (dec1pow k)) f x)).
 Proof.
   (* We prove this by induction on b. *)
 
@@ -505,8 +683,16 @@ Proof.
 
   (* The nil case is trivial. *)
 
+  erewrite oddFromUnfold.
   reflexivity.
 
+  pose (oddFromEven f (applyn (sub (dec1pow k)) f x) (Conb hd odd even)) as oo.
+  assert (coeq oo (oddFromEven f (applyn (sub (dec1pow k)) f x) (Conb hd odd even))) as ooo.
+  reflexivity.
+  rewrite oddFromUnfold in ooo.
+  fold oo.
+  rewrite ooo.
+(*  unfold oo in ooo.*)
   destruct a; simpl.
 
   (* For the odd branch *)
@@ -515,34 +701,40 @@ Proof.
     (bat (oddFromEven f (applyn (sub (dec1pow (S k))) f x) even) b); auto.
   transitivity 
     (bat (oddFromEven f (applyn (S (sub (dec2pow k))) f x) even) b); auto.
-  repeat (repeat f_equal; unfold applyn).
+  repeat (repeat (apply batmor); unfold applyn).
   fold (applyn (sub (dec2pow k)) f x).
-  transitivity (bat (Conb hd odd even) nil); auto.
-  apply H. 
-  unfold ord; auto with arith.
-  repeat (f_equal); auto with arith.
+  apply oddFromMorph.
+  assumption. apply H.
+  apply H0 with (j := nil).
+  simpl; omega. reflexivity. reflexivity.
+  apply batmor.
+  apply oddFromMorph.
+  assumption.
+  apply applynMorph.
+  auto with arith. assumption. reflexivity. reflexivity. reflexivity.
 
   rewrite IHb.
 
   autorewrite with arith.
-  f_equal.
+  apply applynMorph.
   unfold pow; fold (pow 2 k).
   simpl.
   repeat (rewrite mult_plus_distr_r).
   repeat (rewrite mult_plus_distr_l).
-  omega.  
+  omega. assumption.   reflexivity. assumption.
 
   intros.
   transitivity (bat (Conb hd odd even) (false::j)); auto.
-  rewrite H.
+  unfold bat at 2. fold (bat even j). reflexivity.
+  rewrite H0.
   autorewrite with arith.
-  f_equal.
+  apply applynMorph.
   unfold ord; fold (ord j).
   unfold pow; fold (pow 2 k).
   simpl. 
   repeat (rewrite mult_plus_distr_l).
   repeat (rewrite mult_plus_distr_r).
-  simpl. omega.
+  simpl. omega. assumption. reflexivity.
   
   unfold ord; fold (ord j); fold (ord b).
   omega.
@@ -550,107 +742,31 @@ Proof.
   rewrite fmapbat.
 
   transitivity (applyn 1 f (bat (Conb hd odd even) (true::b))); auto.
+  unfold applyn. apply H. auto.
+  unfold bat at 2; fold (bat odd b).
+  reflexivity.
 
-  rewrite H.
-  clear.
+  transitivity (applyn 1 f ((applyn (ord (true::b)) (applyn (pow 2 k) f) (applyn (sub (dec2pow k)) f x)))).
+  apply applynMorph. reflexivity. assumption.
+  apply H0. unfold ord; fold (ord b); omega.
+
   autorewrite with arith.
-  f_equal.
-  
-  unfold ord; fold (ord b).
-  
-  assert (S (sub (dec1pow k)) = pow 2 k). auto using plusMinus.
-  remember (sub (dec1pow k)) as km.
-  destruct km; simpl; omega.
-
-  clear; unfold ord; omega.
-Qed.
+  apply applynMorph.
 (*
-Lemma mainLemma2 :
-  forall b e x f k,
-    (forall j, ord j < ord b -> forall p,
-      bat e j = 
-      applyn (ord j)
-      (applyn (pow 2 k) f) 
-      (applyn (@sub (pow 2 (k+1)) 2 p) f x)) -> forall q r,
-    bat (oddFromEven f 
-      (applyn (@sub (pow 2 k) 1 q) f x) e) b =
-    applyn (ord b)
-    (applyn (pow 2 k) f) 
-    (applyn (@sub (pow 2 k) 1 r) f x).
-Proof.
-  (* We prove this by induction on b. *)
-
-  induction b; destruct e as [hd odd even]; intros. 
-
-  (* The nil case is trivial. *)
-
-  simpl; f_equal; auto; apply subeq; auto.
-
-  destruct a; simpl.
-
-  (* For the odd branch *)
-
-  rewrite <- (@H.
-
-
-(**)
-  transitivity
-    (bat (oddFromEven f (applyn (sub q) f x) even) b); auto.
-  transitivity 
-    (bat (oddFromEven f (applyn (S (sub (dec2pow k))) f x) even) b); auto.
-  repeat (repeat f_equal; unfold applyn).
-  transitivity (bat (Conb hd odd even) nil); auto.
-  apply H. 
-  unfold ord; auto with arith.
-  repeat (f_equal); auto with arith.
-
-  rewrite IHb.
-
-  autorewrite with arith.
-  f_equal.
-  unfold pow; fold (pow 2 k).
-  simpl.
-  repeat (rewrite mult_plus_distr_r).
-  repeat (rewrite mult_plus_distr_l).
-  omega.  
-
-  intros.
-  transitivity (bat (Conb hd odd even) (false::j)); auto.
-  rewrite H.
-  autorewrite with arith.
-  f_equal.
-  unfold ord; fold (ord j).
-  unfold pow; fold (pow 2 k).
-  simpl. 
-  repeat (rewrite mult_plus_distr_l).
-  repeat (rewrite mult_plus_distr_r).
-  simpl. omega.
-  
-  unfold ord; fold (ord j); fold (ord b).
-  omega.
-
-  rewrite fmapbat.
-
-  transitivity (applyn 1 f (bat (Conb hd odd even) (true::b))); auto.
-
   rewrite H.
   clear.
   autorewrite with arith.
   f_equal.
-  
+  *)
   unfold ord; fold (ord b).
   
   assert (S (sub (dec1pow k)) = pow 2 k). auto using plusMinus.
   remember (sub (dec1pow k)) as km.
-  destruct km; simpl; omega.
-
-  clear; unfold ord; omega.
+  destruct km; simpl; omega. assumption. reflexivity.
 Qed.
 
-Check mainLemma.
-*)
 Lemma iter :
-  let P b := forall f x, bat (iterate f x) b = applyn (ord b) f x
+  let P b := forall f x, aext f f -> Aeq (bat (iterate f x) b) (applyn (ord b) f x)
     in forall b, P b.
 Proof.
   intro P.
@@ -658,10 +774,11 @@ Proof.
 
   eapply (well_founded_ind bwf).
   intros b IH.
-  unfold P.
+  unfold P in *.
   intros.
   (* If b is the head element, the proof is trivial. Otherwise, name the head of b "a" and the tail of b "b" *)
   destruct b as [|a b]; auto.
+  simpl. reflexivity.
 
   (*
      Most of the work is done in this helper lemma:
@@ -669,8 +786,8 @@ Proof.
   *)
 
   assert 
-    (bat (oddFromEven f (f x) (ev f x)) b =
-      f (applyn (ord b + (ord b + 0)) f x)) as help.
+    (Aeq (bat (oddFromEven f (f x) (ev f x)) b)
+      (f (applyn (ord b + (ord b + 0)) f x))) as help.
 
   (* 2^1-1 = 1 *)
 
@@ -679,24 +796,26 @@ Proof.
   rewrite mainLemma.
   (* Some simple arithmetic proves the required equality for the application of mainLemma *)
   transitivity (applyn 1 f (applyn (ord b + (ord b + 0)) f x)); auto.
-  repeat (repeat (rewrite applyMul); repeat (rewrite applyAdd)).  
-  f_equal. simpl; omega.
+  repeat (repeat (rewrite applyMul); repeat (rewrite applyAdd)).
+  apply applynMorph. simpl; omega. assumption.  reflexivity.
+  reflexivity. assumption.
 
   (* For the condition on mainLemma, first change the sequence we bat to iterate f x, rather than ev f x *)
   intros.
   transitivity
     (bat (iterate f x) (false::j)); auto.
+  unfold iterate. simpl. reflexivity.
   (* Then apply the induction hypothesis *)
   rewrite IH.
   (* The equality now follows with some arithmetic *)
   repeat (rewrite applyMul); repeat (rewrite applyAdd).
-  f_equal.
-  unfold ord; fold (ord j). simpl; omega.
+  apply applynMorph.
+  unfold ord; fold (ord j). simpl; omega. assumption. reflexivity.
 
   (* The condition of the induction hypothesis follows by arithemtic and a simple case analysis on b and j *)
 
   unfold bacc; unfold ord; fold (ord b); fold (ord j). 
-  destruct a; omega.
+  destruct a; omega. assumption.
 
   (*
      End proof of helper lemma.
@@ -707,15 +826,15 @@ Proof.
   destruct a; simpl.
   (* In the odd branch, once we unfold odd, we can apply the helper lemma. *)
 
-  rewrite odeq; apply help.
+  rewrite odUnfold; apply help.
 
   (* We will transform the even branch into an instance of the odd branch. ev unfolds to fmap od. *) 
-  unfold ev.
+  rewrite evUnfold.
   (* The commutator of bat and fmap f is f *)
-  rewrite fmapbat. f_equal.  
+  rewrite fmapbat. apply H.
 
   (* Now this is just the odd case from above *)
-  rewrite odeq. apply help.
+  rewrite odUnfold. apply help.
 Qed.
 
 Check odeq.
